23ca3f881d4ee507eded4f252554d7f1bbac0886
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-04-10 14:51:00 +0300

    Data loader updated with initialization of the tariffs for tonnage due, wharf due and canal due.

--- src/main/java/flagship/bootstrap/DataLoader.java
+++ src/main/java/flagship/bootstrap/DataLoader.java
@@ -1,12 +1,17 @@
 package flagship.bootstrap;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import flagship.domain.entities.Case;
+import flagship.domain.entities.Port;
 import flagship.domain.entities.Ship;
 import flagship.domain.entities.enums.CallPurpose;
 import flagship.domain.entities.enums.PortArea;
 import flagship.domain.entities.enums.ShipType;
 import flagship.persistence.services.ShipService;
+import flagship.utils.calculators.CanalDueCalculator;
+import flagship.utils.tariffs.CanalDueTariff;
 import flagship.utils.tariffs.TonnageDueTariff;
+import flagship.utils.tariffs.WharfDueTariff;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
@@ -15,8 +20,12 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import static flagship.domain.entities.enums.CallPurpose.*;
 import static flagship.domain.entities.enums.PortArea.*;
@@ -30,47 +39,43 @@ public class DataLoader implements ApplicationRunner {
     private final ShipService shipService;
     private final ObjectMapper objectMapper;
     private final TonnageDueTariff tonnageDueTariff;
+    private final WharfDueTariff wharfDueTariff;
+    private final CanalDueTariff canalDueTariff;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
         initializeTonnageDueTariff();
+        initializeWharfDueTariff();
+        initializeCanalDueTariff();
 
         produceTonnageDueJsonFile();
+
+        File file = new File("src/main/resources/tonnageDueTariff.json");
+        TonnageDueTariff tonnageDueTariff2 = objectMapper.readValue(file, TonnageDueTariff.class);
 
         Ship ship = Ship
                 .builder()
                 .lengthOverall(99.99)
                 .grossTonnage(12345)
-                .type(ShipType.OIL_TANKER)
+                .type(PASSENGER)
                 .build();
 
-        File file = new File("src/main/resources/tonnageDueTariff.json");
-        TonnageDueTariff tonnageDueTariff2 = objectMapper.readValue(file, TonnageDueTariff.class);
+        Port port = Port
+                .builder()
+                .area(FIRST)
+                .build();
 
-        System.out.println();
+        Case activeCase = Case
+                .builder()
+                .ship(ship)
+                .callPurpose(SPECIAL_PURPOSE_PORT_VISIT)
+                .port(port)
+                .callCount(2)
+                .alongsideDaysExpected(3)
+                .build();
 
-
-//
-//        Port port = Port
-//                .builder()
-//                .area(FIRST)
-//                .build();
-//
-//        Case activeCase = Case
-//                .builder()
-//                .ship(ship)
-//                .callPurpose(LOADING)
-//                .port(port)
-//                .callCount(1)
-//                .alongsideDaysExpected(3)
-//                .build();
-//
-//        BigDecimal tonnageDue = TonnageDueCalculator.calculateTonnageDue(activeCase, tonnageDueTariff);
-//        BigDecimal wharfDue = WharfDueCalculator.calculateWharfDue(activeCase);
-//
-
-
+        BigDecimal canalDue = CanalDueCalculator.calculateCanalDue(activeCase,canalDueTariff);
     }
 
     private void initializeTonnageDueTariff() {
@@ -99,12 +104,67 @@ public class DataLoader implements ApplicationRunner {
         discountCoefficientByShipType.put(PASSENGER, 0.4);
         tonnageDueTariff.setDiscountCoefficientsByShipType(discountCoefficientByShipType);
 
-        List<CallPurpose> callPurposesEligibleForDiscount = Arrays.asList(RESUPPLY, RECRUITMENT, POSTAL, REPAIR);
-        tonnageDueTariff.setCallPurposesEligibleForDiscount(callPurposesEligibleForDiscount);
+        Map<CallPurpose, Double> discountCoefficientsByCallPurpose = new HashMap<>();
+        discountCoefficientsByCallPurpose.put(RESUPPLY, 0.65);
+        discountCoefficientsByCallPurpose.put(RECRUITMENT, 0.65);
+        discountCoefficientsByCallPurpose.put(POSTAL, 0.65);
+        discountCoefficientsByCallPurpose.put(REPAIR, 0.65);
+        tonnageDueTariff.setDiscountCoefficientsByCallPurpose(discountCoefficientsByCallPurpose);
 
         tonnageDueTariff.setCallCountThreshold(4);
         tonnageDueTariff.setCallCountDiscountCoefficient(0.7);
-        tonnageDueTariff.setCallPurposeDiscountCoefficient(0.65);
+    }
+
+    private void initializeWharfDueTariff() {
+
+        Map<CallPurpose, Double> discountCoefficientsByCallPurpose = new HashMap<>();
+        discountCoefficientsByCallPurpose.put(RESUPPLY, 0.5);
+        discountCoefficientsByCallPurpose.put(RECRUITMENT, 0.5);
+        discountCoefficientsByCallPurpose.put(POSTAL, 0.5);
+        discountCoefficientsByCallPurpose.put(REPAIR, 0.5);
+        wharfDueTariff.setDiscountCoefficientsByCallPurpose(discountCoefficientsByCallPurpose);
+
+        Set<ShipType> shipTypesNotEligibleForDiscount = new HashSet<>();
+        shipTypesNotEligibleForDiscount.add(MILITARY);
+        wharfDueTariff.setShipTypesNotEligibleForDiscount(shipTypesNotEligibleForDiscount);
+
+        Map<ShipType, Double> wharfDuesByShipType = new HashMap<>();
+        wharfDuesByShipType.put(MILITARY, 0.5);
+        wharfDueTariff.setWharfDuesByShipType(wharfDuesByShipType);
+
+        wharfDueTariff.setDefaultWharfDue(0.10);
+    }
+
+    private void initializeCanalDueTariff() {
+
+        Map<PortArea, Double> canalDuesByPortArea = new HashMap<>();
+        canalDuesByPortArea.put(FIRST, 0.04);
+        canalDuesByPortArea.put(SECOND, 0.13);
+        canalDuesByPortArea.put(THIRD, 0.04);
+        canalDuesByPortArea.put(FOURTH, 0.07);
+        canalDueTariff.setCanalDuesByPortArea(canalDuesByPortArea);
+
+        Map<ShipType, Double> discountCoefficientByShipType = new HashMap<>();
+        discountCoefficientByShipType.put(PASSENGER, 0.5);
+        canalDueTariff.setDiscountCoefficientByShipType(discountCoefficientByShipType) ;
+
+        Map<PortArea, Double> discountCoefficientsByPortAreaForContainers = new HashMap<>();
+        discountCoefficientsByPortAreaForContainers.put(FIRST, 0.25);
+        discountCoefficientsByPortAreaForContainers.put(SECOND, 0.74);
+        discountCoefficientsByPortAreaForContainers.put(THIRD, 0.25);
+        discountCoefficientsByPortAreaForContainers.put(FOURTH, 0.74);
+        canalDueTariff.setDiscountCoefficientsByPortAreaForContainers(discountCoefficientsByPortAreaForContainers);
+
+        Map<PortArea, Double> discountCoefficientsByPortAreaPerCallCountForContainers = new HashMap<>();
+        discountCoefficientsByPortAreaPerCallCountForContainers.put(FIRST, 0.20);
+        discountCoefficientsByPortAreaPerCallCountForContainers.put(SECOND, 0.59);
+        discountCoefficientsByPortAreaPerCallCountForContainers.put(THIRD, 0.20);
+        discountCoefficientsByPortAreaPerCallCountForContainers.put(FOURTH, 0.59);
+        canalDueTariff.setDiscountCoefficientsByPortAreaPerCallCountForContainers(discountCoefficientsByPortAreaPerCallCountForContainers);
+
+        canalDueTariff.setCallCountThreshold(3);
+        canalDueTariff.setDefaultCallCountDiscountCoefficient(0.8);
+
     }
 
     private void produceTonnageDueJsonFile() throws IOException {
