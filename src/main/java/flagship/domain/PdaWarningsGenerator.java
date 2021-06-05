b217459c958278001ace35f59c85895ea9a6e968
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-06-05 08:45:04 +0300

    Refactored the Warning generator logic - now the generator evaluates the warning events and do not depend on the Warning types collection of enums. Refactored the warnings for the wharf due.

--- src/main/java/flagship/domain/PdaWarningsGenerator.java
+++ src/main/java/flagship/domain/PdaWarningsGenerator.java
@@ -3,7 +3,6 @@ package flagship.domain;
 import flagship.domain.cases.dto.PdaCase;
 import flagship.domain.cases.entities.Warning;
 import flagship.domain.factories.TariffsFactory;
-import flagship.domain.renders.pda.PdaElementsFactory;
 import flagship.domain.tariffs.servicedues.MooringDueTariff;
 import flagship.domain.tariffs.servicedues.PilotageDueTariff;
 import flagship.domain.tariffs.servicedues.TugDueTariff;
@@ -15,34 +14,22 @@ import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static flagship.domain.renders.pda.PdaElementsFactory.DueType.*;
 import static flagship.domain.PdaWarningsGenerator.WarningType.*;
 import static flagship.domain.calculators.DueCalculator.CalculatorType.*;
+import static flagship.domain.renders.pda.elements.PdaElementsFactory.DueType;
+import static flagship.domain.renders.pda.elements.PdaElementsFactory.DueType.*;
 
 // todo: Confirm, that predefining UUID id is not a problem for Hibernate
-
-// todo: add warning that the PDA is not precise at calculating wharf, pilotage and tug dues and
-// moorning/unmoorning
-// because ETA and ETD are not provided
-
-// todo: Add warning regarding the wharf due - if between ETA and ETD - the stay of the ship will be
-// increased.
-// This must increase the total and must generate warning. The warning message will say, that the
-// expected wharf due
-// is increased.
 
 // todo: Add warning if ship is special and ETA and ETD are not being provided. If stay for more
 // than the end of the month
 // the exact increase can not be evaluated
-
-// todo: generate warnings for mooring due
-
-// If alongsideDays not provided - the agent will enter value based on his experiences.
 
 // todo: add warning for car due increase for port
 
@@ -55,69 +42,90 @@ public class PdaWarningsGenerator {
 
   public Set<Warning> generateWarnings() {
 
-    LocalDate eta = source.getEstimatedDateOfArrival();
-    LocalDate etd = source.getEstimatedDateOfDeparture();
+    Optional<LocalDate> eta = Optional.ofNullable(source.getEstimatedDateOfArrival());
+    Optional<LocalDate> etd = Optional.ofNullable(source.getEstimatedDateOfDeparture());
 
     final Set<Warning> warnings = new LinkedHashSet<>();
 
-    if (source.getWarningTypes().contains(ETA_IS_HOLIDAY)) {
-      warnings.addAll(getHolidayWarnings(eta, ETA_IS_HOLIDAY));
+    if (eta.isPresent()) {
+      if (isHoliday(eta)) {
+        warnings.addAll(getHolidayWarnings(eta, ETA_IS_HOLIDAY));
+      }
+    } else {
+      warnings.addAll(getMissingDateWarnings(ETA_NOT_PROVIDED));
     }
-    if (source.getWarningTypes().contains(ETD_IS_HOLIDAY)) {
-      warnings.addAll(getHolidayWarnings(etd, ETD_IS_HOLIDAY));
+
+    if (etd.isPresent()) {
+      if (isHoliday(etd)) {
+        warnings.addAll(getHolidayWarnings(etd, ETD_IS_HOLIDAY));
+      }
+    } else {
+      warnings.addAll(getMissingDateWarnings(ETD_NOT_PROVIDED));
     }
-    if (source.getWarningTypes().contains(ETA_NOT_PROVIDED)) {
-      warnings.addAll(getDateMissingWarnings(ETA_NOT_PROVIDED));
-    }
-    if (source.getWarningTypes().contains(ETD_NOT_PROVIDED)) {
-      warnings.addAll(getDateMissingWarnings(ETD_NOT_PROVIDED));
-    }
-    if (source.getWarningTypes().contains(ETA_IS_HOLIDAY)
-        && source.getWarningTypes().contains(ETD_IS_HOLIDAY)) {
-      warnings.addAll(getIntermediateHolidayWarningsForWharfDue(eta, etd));
-    }
+
+    warnings.addAll(getHolidayWarningsForWharfDue(etd));
 
     return warnings;
   }
 
-  private Set<Warning> getHolidayWarnings(final LocalDate date, final WarningType warningType) {
-    return Stream.of(PILOTAGE_DUE, TUG_DUE, MOORING_DUE, WHARF_DUE)
-        .map(due -> getNewWarning(date, warningType, due, getFactor(due)))
+  private Set<Warning> getHolidayWarnings(
+      final Optional<LocalDate> date, final WarningType warningType) {
+    return Stream.of(PILOTAGE_DUE, TUG_DUE, MOORING_DUE)
+        .map(due -> getNewWarning(date.get(), warningType, due, getFactor(due)))
         .collect(Collectors.toCollection(LinkedHashSet::new));
   }
 
-  private Set<Warning> getDateMissingWarnings(final WarningType warningType) {
-    return Stream.of(PILOTAGE_DUE, TUG_DUE, MOORING_DUE, WHARF_DUE)
+  private Set<Warning> getMissingDateWarnings(final WarningType warningType) {
+    return Stream.of(PILOTAGE_DUE, TUG_DUE, MOORING_DUE)
         .map(due -> getNewWarning(null, warningType, due, BigDecimal.ZERO))
         .collect(Collectors.toCollection(LinkedHashSet::new));
   }
 
-  private Set<Warning> getIntermediateHolidayWarningsForWharfDue(
-      final LocalDate eta, final LocalDate etd) {
+  private Set<Warning> getHolidayWarningsForWharfDue(final Optional<LocalDate> etd) {
 
     PilotageDueTariff pilotageDueTariff =
         (PilotageDueTariff) tariffsFactory.getTariff(PILOTAGE_DUE_CALCULATOR);
 
-    final Set<Warning> warnings = new LinkedHashSet<>();
     final Set<LocalDate> holidays = pilotageDueTariff.getHolidayCalendar();
 
-    LocalDate date = eta.plusDays(1);
+    final Set<Warning> warnings = new LinkedHashSet<>();
 
-    while (!date.equals(etd)) {
-      if (holidays.contains(date)) {
+    if (etd.isPresent()) {
+
+      LocalDate date = etd.get();
+
+      if (holidays.contains(date) && !holidays.contains(date.plusDays(1))) {
         warnings.add(getNewWarning(date, HOLIDAY, WHARF_DUE, BigDecimal.ONE));
+        return warnings;
       }
-      date = date.plusDays(1);
-    }
 
+      if (holidays.contains(date)) {
+        while (holidays.contains(date)) {
+          warnings.add(getNewWarning(date, HOLIDAY, WHARF_DUE, BigDecimal.ONE));
+          date = date.plusDays(1);
+          if (!holidays.contains(date)) {
+            break;
+          }
+        }
+      }
+    } else {
+      warnings.add(
+          Warning.builder()
+              .dueType(WHARF_DUE)
+              .warningType(ETD_NOT_PROVIDED)
+              .warningFactor(BigDecimal.ZERO)
+              .build());
+    }
     return warnings;
   }
 
-  private BigDecimal getFactor(final PdaElementsFactory.DueType due) {
+  private BigDecimal getFactor(final DueType due) {
 
     TugDueTariff tugDueTariff = (TugDueTariff) tariffsFactory.getTariff(TUG_DUE_CALCULATOR);
+
     MooringDueTariff mooringDueTariff =
         (MooringDueTariff) tariffsFactory.getTariff(MOORING_DUE_CALCULATOR);
+
     PilotageDueTariff pilotageDueTariff =
         (PilotageDueTariff) tariffsFactory.getTariff(PILOTAGE_DUE_CALCULATOR);
 
@@ -146,7 +154,7 @@ public class PdaWarningsGenerator {
   private Warning getNewWarning(
       final LocalDate date,
       final WarningType warningType,
-      final PdaElementsFactory.DueType dueType,
+      final DueType dueType,
       final BigDecimal factor) {
     return Warning.builder()
         .id(UUID.randomUUID())
@@ -156,6 +164,14 @@ public class PdaWarningsGenerator {
         .warningFactor(factor)
         .dateCreated(Timestamp.valueOf(LocalDateTime.now()))
         .build();
+  }
+
+  private boolean isHoliday(final Optional<LocalDate> date) {
+
+    PilotageDueTariff pilotageDueTariff =
+        (PilotageDueTariff) tariffsFactory.getTariff(PILOTAGE_DUE_CALCULATOR);
+
+    return date.filter(d -> pilotageDueTariff.getHolidayCalendar().contains(d)).isPresent();
   }
 
   public enum WarningType {
@@ -169,5 +185,4 @@ public class PdaWarningsGenerator {
     SPECIAL_PILOTAGE_CARGO,
     DANGEROUS_TUG_CARGO,
   }
-
 }
--- src/test/java/flagship/domain/calculators/PdaWarningsGeneratorTest.java
+++ src/test/java/flagship/domain/calculators/PdaWarningsGeneratorTest.java
@@ -3,29 +3,27 @@ package flagship.domain.calculators;
 import flagship.domain.PdaWarningsGenerator;
 import flagship.domain.PdaWarningsGenerator.WarningType;
 import flagship.domain.cases.dto.PdaCase;
+import flagship.domain.cases.entities.Warning;
 import flagship.domain.factories.TariffsFactory;
 import flagship.domain.tariffs.Tariff;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.util.EnumMap;
-import java.util.EnumSet;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static flagship.domain.PdaWarningsGenerator.WarningType.*;
 import static flagship.domain.calculators.DueCalculator.CalculatorType;
 import static flagship.domain.calculators.DueCalculator.CalculatorType.*;
+import static flagship.domain.renders.pda.elements.PdaElementsFactory.DueType.WHARF_DUE;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@Disabled("Disabled until the warning model is clarified")
 @DisplayName("Pilotage due warning generator tests")
 class PdaWarningsGeneratorTest extends TariffsInitializer {
 
-  private static TariffsFactory tariffsFactory;
+  private static final TariffsFactory tariffsFactory = new TariffsFactory();
   private static PdaWarningsGenerator warningsGenerator;
   PdaCase testCase;
 
@@ -50,45 +48,41 @@ class PdaWarningsGeneratorTest extends TariffsInitializer {
     tariffs.put(OVERTIME_DUE_CALCULATOR, agencyDuesTariff);
     tariffsFactory.setTariffs(tariffs);
 
-    testCase =
-        PdaCase.builder()
-            .estimatedDateOfArrival(LocalDate.now())
-            .estimatedDateOfDeparture(LocalDate.now().plusDays(5))
-            .warnings(new HashSet<>())
-            .build();
+    testCase = PdaCase.builder().warningTypes(new HashSet<>()).warnings(new HashSet<>()).build();
   }
 
-  @DisplayName("Should generate 4 eta detected warnings")
+  @DisplayName("Should generate 3 eta detected warnings")
   @Test
   void testGeneratesEtaDetectedWarnings() {
 
-    testCase.getWarningTypes().addAll(EnumSet.of(ETA_IS_HOLIDAY, ETD_NOT_PROVIDED));
+    testCase.setEstimatedDateOfArrival(LocalDate.of(2021, 12, 28));
 
     warningsGenerator = new PdaWarningsGenerator(testCase, tariffsFactory);
 
     final long warningsCount = getCountForWarning(ETA_IS_HOLIDAY);
 
-    assertThat(warningsCount).isEqualTo(4);
+    assertThat(warningsCount).isEqualTo(3);
   }
 
-  @DisplayName("Should generate 4 etd detected warnings")
+  @DisplayName("Should generate 3 etd detected warnings")
   @Test
   void testGeneratesEtdDetectedWarnings() {
 
-    testCase.getWarningTypes().addAll(EnumSet.of(ETD_IS_HOLIDAY, ETA_NOT_PROVIDED));
+    testCase.setEstimatedDateOfDeparture(LocalDate.of(2021, 12, 28));
 
     warningsGenerator = new PdaWarningsGenerator(testCase, tariffsFactory);
 
     long warningsCount = getCountForWarning(ETD_IS_HOLIDAY);
 
-    assertThat(warningsCount).isEqualTo(4);
+    assertThat(warningsCount).isEqualTo(3);
   }
 
-  @DisplayName("Should generate 8 holiday detected warnings")
+  @DisplayName("Should generate 6 holiday detected warnings")
   @Test
   void testGeneratesEtaAndEtdDetectedWarnings() {
 
-    testCase.getWarningTypes().addAll(EnumSet.of(ETA_IS_HOLIDAY, ETD_IS_HOLIDAY));
+    testCase.setEstimatedDateOfArrival(LocalDate.of(2021, 12, 28));
+    testCase.setEstimatedDateOfDeparture(LocalDate.of(2021, 12, 28));
 
     warningsGenerator = new PdaWarningsGenerator(testCase, tariffsFactory);
 
@@ -100,27 +94,27 @@ class PdaWarningsGeneratorTest extends TariffsInitializer {
                         || warning.getWarningType() == ETD_IS_HOLIDAY)
             .count();
 
-    assertThat(warningsCount).isEqualTo(8);
+    assertThat(warningsCount).isEqualTo(6);
   }
 
-  @DisplayName("Should generate 4 eta not provided warnings")
+  @DisplayName("Should generate 3 eta not provided warnings")
   @Test
   void testGeneratesEtaNotProvidedWarnings() {
 
-    testCase.getWarningTypes().addAll(EnumSet.of(ETA_NOT_PROVIDED, ETD_IS_HOLIDAY));
+    testCase.setEstimatedDateOfDeparture(LocalDate.of(2021, 12, 28));
 
     warningsGenerator = new PdaWarningsGenerator(testCase, tariffsFactory);
 
     final long warningsCount = getCountForWarning(ETA_NOT_PROVIDED);
 
-    assertThat(warningsCount).isEqualTo(4);
+    assertThat(warningsCount).isEqualTo(3);
   }
 
   @DisplayName("Should generate 4 etd not provided warnings")
   @Test
   void testGeneratesEtdNotProvidedWarnings() {
 
-    testCase.getWarningTypes().addAll(EnumSet.of(ETD_NOT_PROVIDED, ETA_IS_HOLIDAY));
+    testCase.setEstimatedDateOfArrival(LocalDate.of(2021, 12, 28));
 
     warningsGenerator = new PdaWarningsGenerator(testCase, tariffsFactory);
 
@@ -129,7 +123,7 @@ class PdaWarningsGeneratorTest extends TariffsInitializer {
     assertThat(warningsCount).isEqualTo(4);
   }
 
-  @DisplayName("Should generate 8 eta and etd not provided warnings")
+  @DisplayName("Should generate 7 eta and etd not provided warnings")
   @Test
   void testGeneratesEtaAndEtdNotProvidedWarnings() {
 
@@ -145,12 +139,54 @@ class PdaWarningsGeneratorTest extends TariffsInitializer {
                         || warning.getWarningType() == ETD_NOT_PROVIDED)
             .count();
 
-    assertThat(warningsCount).isEqualTo(8);
+    assertThat(warningsCount).isEqualTo(7);
   }
 
+  @DisplayName("Should generate 1 warning message")
   @Test
-  void testGenerateIntermediateHolidayWarnings() {
-    testCase.getWarningTypes().addAll(EnumSet.of(ETA_IS_HOLIDAY, ETD_IS_HOLIDAY));
+  void testGeneratesOneHolidayWarningForWharfDue() {
+
+    testCase.setEstimatedDateOfArrival(LocalDate.of(2021, 5, 1));
+    testCase.setEstimatedDateOfDeparture(LocalDate.of(2021, 5, 4));
+
+    warningsGenerator = new PdaWarningsGenerator(testCase, tariffsFactory);
+
+    Set<Warning> warnings =
+        warningsGenerator.generateWarnings().stream()
+            .filter(warning -> warning.getDueType() == WHARF_DUE)
+            .collect(Collectors.toSet());
+
+    assertThat(warnings.size()).isEqualTo(1);
+  }
+
+  @DisplayName("Should generate 3 warning message")
+  @Test
+  void testGenerateMultipleHolidayWarningForWharfDue() {
+
+    testCase.setEstimatedDateOfDeparture(LocalDate.of(2021, 5, 2));
+
+    warningsGenerator = new PdaWarningsGenerator(testCase, tariffsFactory);
+
+    Set<Warning> warnings =
+        warningsGenerator.generateWarnings().stream()
+            .filter(warning -> warning.getDueType() == WHARF_DUE)
+            .collect(Collectors.toSet());
+
+    assertThat(warnings.size()).isEqualTo(3);
+  }
+
+  @DisplayName("Should generate 1 warning message for missing ETD")
+  @Test
+  void testGeneratesOneWarningForMissingEtdWharfDue() {
+
+    warningsGenerator = new PdaWarningsGenerator(testCase, tariffsFactory);
+
+    Set<Warning> warnings =
+            warningsGenerator.generateWarnings().stream()
+                    .filter(warning -> warning.getDueType() == WHARF_DUE)
+                    .collect(Collectors.toSet());
+
+    assertThat(warnings.size()).isEqualTo(1);
   }
 
   private long getCountForWarning(final WarningType warning) {
